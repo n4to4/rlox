e81229bf4862ee0726b6aa34694f8344a98872db
n4to4 <n4to4k@gmail.com>
2021-09-02 21:39:18 +0900

    snapshot

--- src/chunk.rs
+++ src/chunk.rs
@@ -1,33 +1,46 @@
+use crate::value::{Value, ValueArray};
+
 #[derive(Debug, Clone, Copy)]
 pub(crate) enum OpCode {
+    Constant(u8),
     Return,
-}
-
-impl OpCode {
-    pub(crate) fn disassemble(&self, offset: usize) {
-        println!("{:04} {:?}", offset, self);
-    }
 }
 
 #[derive(Debug)]
 pub(crate) struct Chunk {
     code: Vec<OpCode>,
+    constants: ValueArray,
 }
 
 impl Chunk {
     pub(crate) fn new() -> Self {
-        Chunk { code: Vec::new() }
+        Chunk {
+            code: Vec::new(),
+            constants: ValueArray::new(),
+        }
     }
 
     pub(crate) fn write_chunk(&mut self, byte: OpCode) {
         self.code.push(byte);
     }
 
+    pub(crate) fn add_constant(&mut self, value: Value) {
+        self.constants.write_value_array(value);
+        let offset = self.constants.len() - 1;
+        self.write_chunk(OpCode::Constant(offset as u8));
+    }
+
     pub(crate) fn disassemble(&self, name: &str) {
         println!("=== {} ===", name);
 
         for (offset, c) in self.code.iter().enumerate() {
-            c.disassemble(offset);
+            print!("{:04} ", offset);
+            match *c {
+                OpCode::Return => println!("{:?}", OpCode::Return),
+                OpCode::Constant(off) => {
+                    println!("Constant {:}", self.constants[off].0);
+                }
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 mod chunk;
+mod value;
 
 use chunk::{Chunk, OpCode};
+use value::Value;
 
 fn main() {
     let mut chunk = Chunk::new();
+    chunk.add_constant(Value(1.2));
     chunk.write_chunk(OpCode::Return);
 
     dbg!(&chunk);
