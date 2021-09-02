0c40a2fc1225cd976f19a9ec9f7b260338380eb6
n4to4 <n4to4k@gmail.com>
2021-09-02 20:28:24 +0900

    debug

--- src/chunk.rs
+++ src/chunk.rs
@@ -1,19 +1,33 @@
 #[derive(Debug, Clone, Copy)]
-enum OpCode {
+pub(crate) enum OpCode {
     Return,
 }
 
+impl OpCode {
+    pub(crate) fn disassemble(&self, offset: usize) {
+        println!("{:04} {:?}", offset, self);
+    }
+}
+
 #[derive(Debug)]
-struct Chunk {
+pub(crate) struct Chunk {
     code: Vec<OpCode>,
 }
 
 impl Chunk {
-    fn new() -> Self {
+    pub(crate) fn new() -> Self {
         Chunk { code: Vec::new() }
     }
 
-    fn write_chunk(&mut self, byte: OpCode) {
+    pub(crate) fn write_chunk(&mut self, byte: OpCode) {
         self.code.push(byte);
+    }
+
+    pub(crate) fn disassemble(&self, name: &str) {
+        println!("=== {} ===", name);
+
+        for (offset, c) in self.code.iter().enumerate() {
+            c.disassemble(offset);
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 mod chunk;
 
-fn main() {}
+use chunk::{Chunk, OpCode};
+
+fn main() {
+    let mut chunk = Chunk::new();
+    chunk.write_chunk(OpCode::Return);
+
+    dbg!(&chunk);
+
+    chunk.disassemble("test");
+}
