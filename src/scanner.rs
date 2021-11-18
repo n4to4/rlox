6c39d64e98d7fba95e51ba645a31ced27b592251
n4to4 <n4to4k@gmail.com>
2021-11-18 19:13:45 +0900

    scanner

--- src/scanner.rs
+++ src/scanner.rs
@@ -42,11 +42,95 @@ impl<'src> Scanner<'src> {
         }
     }
 
+    fn advance(&mut self) -> char {
+        self.current += 1;
+        self.source.as_bytes()[self.current - 1] as char
+    }
+
+    fn matches(&mut self, expected: char) -> bool {
+        if self.is_at_end() {
+            return false;
+        }
+        if self.source.as_bytes()[self.current] as char != expected {
+            return false;
+        }
+        self.current += 1;
+        true
+    }
+
+    fn peek(&self) -> char {
+        self.source.as_bytes()[self.current] as char
+    }
+
+    fn skip_whitespace(&mut self) {
+        loop {
+            match self.peek() {
+                ' ' | '\r' | '\t' => {
+                    self.advance();
+                }
+                '\n' => {
+                    self.line += 1;
+                    self.advance();
+                }
+                _ => return,
+            }
+        }
+    }
+
     pub fn scan_token(&mut self) -> Token {
         self.start = self.current;
         if self.is_at_end() {
             return self.make_token(TokenType::Eof);
         }
+
+        let c = self.advance();
+        match c {
+            '(' => return self.make_token(TokenType::LeftParen),
+            ')' => return self.make_token(TokenType::RightParen),
+            '{' => return self.make_token(TokenType::LeftBrace),
+            '}' => return self.make_token(TokenType::RightBrace),
+            ';' => return self.make_token(TokenType::Semicolon),
+            ',' => return self.make_token(TokenType::Comma),
+            '.' => return self.make_token(TokenType::Dot),
+            '-' => return self.make_token(TokenType::Minus),
+            '+' => return self.make_token(TokenType::Plus),
+            '/' => return self.make_token(TokenType::Slash),
+            '*' => return self.make_token(TokenType::Star),
+            '!' => {
+                let token = if self.matches('=') {
+                    TokenType::BangEqual
+                } else {
+                    TokenType::Bang
+                };
+                return self.make_token(token);
+            }
+            '=' => {
+                let token = if self.matches('=') {
+                    TokenType::EqualEqual
+                } else {
+                    TokenType::Equal
+                };
+                return self.make_token(token);
+            }
+            '<' => {
+                let token = if self.matches('=') {
+                    TokenType::LessEqual
+                } else {
+                    TokenType::Less
+                };
+                return self.make_token(token);
+            }
+            '>' => {
+                let token = if self.matches('=') {
+                    TokenType::GreaterEqual
+                } else {
+                    TokenType::Greater
+                };
+                return self.make_token(token);
+            }
+            _ => {}
+        }
+
         self.error_token("Unexpected character.")
     }
 
